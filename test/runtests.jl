899b1c60397f0f9c0d2e5d961b9dbea916c977b7
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-01-07 18:11:40 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
 using BioJournals
 using Test
+using BioSequences
+using DataStructures
 
 @testset "BioJournals.jl" begin
-    # Write your tests here.
+    reference_seq = LongDNA{4}("AGATCGAGCGAGCTAGCGACTCAG")
+    deltaMap = [SortedDict{Int, JournalEntry}() for _ in 1:10]
+    jst = JournaledString(reference_seq, deltaMap)
+    add_delta!(jst.deltaMap, [1, 2], DeltaTypeIns, 8, "CGTA")
+    add_delta!(jst.deltaMap, [4, 9], DeltaTypeSnp, 10, 'C')
+    add_delta!(jst.deltaMap, [8], DeltaTypeIns, 24, "NNNNN")
+    @test build_sequences(jst) == "Sequence 1: AGATCGAATGCGCGAGCTAGCGACTCAG\n" *
+                                "Sequence 2: AGATCGAATGCGCGAGCTAGCGACTCAG\n" *
+                                "Sequence 3: AGATCGAGCGAGCTAGCGACTCAG\n" *
+                                "Sequence 4: AGATCGAGCCAGCTAGCGACTCAG\n" *
+                                "Sequence 5: AGATCGAGCGAGCTAGCGACTCAG\n" *
+                                "Sequence 6: AGATCGAGCGAGCTAGCGACTCAG\n" *
+                                "Sequence 7: AGATCGAGCGAGCTAGCGACTCAG\n" *
+                                "Sequence 8: AGATCGAGCGAGCTAGCGACTCANNNNNG\n" *
+                                "Sequence 9: AGATCGAGCCAGCTAGCGACTCAG\n" *
+                                "Sequence 10: AGATCGAGCGAGCTAGCGACTCAG\n"
 end
