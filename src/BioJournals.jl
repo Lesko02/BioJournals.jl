8db7f1936a9c0b995ea66857b2c2437616af3141
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-01-28 17:02:09 +0100

    Update BioJournals.jl

--- src/BioJournals.jl
+++ src/BioJournals.jl
@@ -6,10 +6,12 @@ using FASTX
 
 include("structs.jl")
 include("functions.jl")
+include("api.jl")
 
-export insert!, delete_at!, structure_variation!, copy_number_variation!,
- JournalEntry, JournaledString, add_delta!, remove_delta!, apply_delta,
- print_sequences, build_sequences, print_deltas, get_mutation_history,
- get_mutation_interval, get_sequences_at_time, simulate_mutation!,
- remove_mutation!, is_equal
+export DeltaType, DeltaTypeDel, DeltaTypeIns, DeltaTypeSnp, DeltaTypeSV,
+ DeltaTypeCNV, insert!, delete_at!, structure_variation!,
+ copy_number_variation!, JournalEntry, JournaledString, add_delta!,
+ remove_delta!, apply_delta, print_sequences, build_sequences, print_deltas,
+ get_mutation_history, get_mutation_interval, get_sequences_at_time,
+ simulate_mutation!, remove_mutation!, is_equal
 end
