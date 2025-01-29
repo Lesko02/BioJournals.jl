884c56ef061e1f4fb1e5f5b26fb78cc59c016979
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-01-29 16:25:35 +0100

    created function structures

---
+++ src/algorithms.jl
@@ -0,0 +1,23 @@
+function horspool_find(jss::JournaledString, needle::LongDNA{4})
+    # Yet to be coded
+end
+
+function horspool_findfind(jst::JSTree, needle::LongDNA{4})
+    # Yet to be coded
+end
+
+function myers_ukkoken_find(jss::JournaledString, needle::LongDNA{4})
+    # Yet to be coded
+end
+
+function myers_ukkoken_find(jst::JSTree, needle::LongDNA{4})
+    # Yet to be coded
+end
+
+function naive_find(jss::JournaledString, needle::LongDNA{4})
+    # Yet to be coded
+end
+
+function naive_find(jst::JSTree, needle::LongDNA{4})
+    # Yet to be coded
+end
