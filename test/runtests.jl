dd644491f1fe3bf7e0e542fdbe1c8ecc1b691c99
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-01-31 11:55:15 +0100

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,19 @@ using DataStructures
     add_delta!(jst, [4, 9], DeltaTypeSnp, 10, 'C')
     add_delta!(jst, [8], DeltaTypeIns, 24, "NNNNN")
     
+    js1 = JournaledString(reference_seq, [SortedDict{Int, JournalEntry}() for _ in 1:10], 0)
+    js2 = JournaledString(reference_seq, [SortedDict{Int, JournalEntry}() for _ in 1:10], 0)
+    add_delta!(js1, [1, 2], DeltaTypeIns, 8, "CGTA")
+    add_delta!(js2, [1, 2], DeltaTypeIns, 8, "CGTA")
+
+    @test (js1==js2) == false
+
+    @test (js1==js1) == true
+
+    @test is_equal(js1, js2) == true
+    
+    @test Base.isequal(js1, js2) == false
+
     @test build_sequences(jst) ==
         "Sequence 1: AGATCGAATGCGCGAGCTAGCGACTCAG\n" *
         "Sequence 2: AGATCGAATGCGCGAGCTAGCGACTCAG\n" *
@@ -25,6 +38,8 @@ using DataStructures
         "Sequence 8: AGATCGAGCGAGCTAGCGACTCANNNNNG\n" *
         "Sequence 9: AGATCGAGCCAGCTAGCGACTCAG\n" *
         "Sequence 10: AGATCGAGCGAGCTAGCGACTCAG\n"
+    
+
 end
 
 ### runtests.jl ends here.
