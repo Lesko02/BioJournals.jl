6211cd4b5822884cfdc60705a3308ca3023959f5
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-02-03 10:31:38 +0100

    added naive_find(JSS)

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -14,8 +14,24 @@ function myers_ukkoken_find(jst::JSTree, needle::LongDNA{4})
     # Yet to be coded
 end
 
-function naive_find(jss::JournaledString, needle::LongDNA{4})
-    # Yet to be coded
+function naive_search(jss::JournaledString, needle::LongDNA )
+
+    query = ExactSearchQuery(needle)
+    vector = UnitRange{Int}[]
+    for i in 1:length(jss.deltaMap)
+        empty!(vector)
+        seq = apply_delta(jss.reference, jss.deltaMap[i])
+        vector = BioSequences.findall(query, seq)
+
+        if isempty(vector)
+            println("No match at Deltamap N° $i")
+        else
+            println("Match at Deltamap N° $i")
+            print(" Ranges: ", vector)
+        end
+
+    end
+
 end
 
 function naive_find(jst::JSTree, needle::LongDNA{4})
