f9c30def20e723572593e6e039f1181921cf7604
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-03-07 11:30:26 +0100

    fixed exports in BioJournals.jl

--- src/BioJournals.jl
+++ src/BioJournals.jl
@@ -9,10 +9,12 @@ include("api.jl")
 include("functions.jl")
 include("algorithms.jl")
 
-export DeltaType, DeltaTypeDel, DeltaTypeIns, DeltaTypeSnp, DeltaTypeSV,
- DeltaTypeCNV, insert!, delete_at!, structure_variation!,
- copy_number_variation!, JournalEntry, JournaledString, add_delta!,
- remove_delta!, apply_delta, print_sequences, build_sequences, print_deltas,
- get_mutation_history, get_mutation_interval, get_sequences_at_time,
- simulate_mutation!, remove_mutation!, is_equal
+export DeltaType, DeltaTypeDel, DeltaTypeIns, DeltaTypeSnp, DeltaTypeSV, 
+DeltaTypeCNV, JournalEntry, JournaledString, JSTree, JSTNode, DeltaMap,
+insert!, delete_at!, structure_variation!, copy_number_variation!,
+flatten, print_tree, print_sequences, build_sequences, print_deltas,
+add_delta!, remove_delta!, apply_delta, add_node, remove_node!,
+approximate_findall, approximate_search, exact_search,
+get_mutation_history, get_mutation_interval, get_sequences_at_time,
+simulate_mutation!, remove_mutation!, is_equal, print_results
 end
