58b24ab9898eadefa213f901a6e4d34f8f376091
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-03-19 15:41:04 +0100

    fixed exports

--- src/BioJournals.jl
+++ src/BioJournals.jl
@@ -16,5 +16,5 @@ flatten, print_tree, print_sequences, build_sequences, print_deltas,
 add_delta!, remove_delta!, apply_delta, add_node, remove_node!,
 approximate_findall, approximate_search, exact_search,
 get_mutation_history, get_mutation_interval, get_sequences_at_time,
-simulate_mutation!, remove_mutation!, is_equal, print_results
+simulate_mutation!, remove_mutation!, is_equal, print_results, trim_node
 end
