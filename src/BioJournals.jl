3ac90f7b0b30824336469edfaad78e9c30cb90fc
Ethan Gabriel Leskovec <e.leskovec@campus.unimib.it>
2025-03-26 17:14:45 +0100

    fixed imports, fixed exports

--- src/BioJournals.jl
+++ src/BioJournals.jl
@@ -3,6 +3,7 @@ module BioJournals
 using BioSequences
 using DataStructures
 using FASTX
+using CodecZlib
 
 include("structs.jl")
 include("api.jl")
@@ -18,5 +19,5 @@ add_delta!, remove_delta!, apply_delta, add_node, remove_node!,
 approximate_findall, approximate_search, exact_search,
 get_mutation_history, get_mutation_interval, get_sequences_at_time,
 simulate_mutation!, remove_mutation!, is_equal, print_results, trim_node,
-load_fasta
+load_fasta, write_fasta, load_fastq, write_fastq
 end
