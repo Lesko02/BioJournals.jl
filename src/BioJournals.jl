cad80a7f1dc46b6a51e54a3542783a4470cd00bd
Marco Antoniotti <marco.antoniotti@unimib.it>
2025-03-31 14:29:09 +0200

    Minor cosmetics changes.

--- src/BioJournals.jl
+++ src/BioJournals.jl
@@ -1,3 +1,5 @@
+### BioJournals.jl
+
 module BioJournals
 
 using BioSequences
@@ -12,12 +14,16 @@ include("algorithms.jl")
 include("io.jl")
 
 export DeltaType, DeltaTypeDel, DeltaTypeIns, DeltaTypeSnp, DeltaTypeSV, 
-DeltaTypeCNV, JournalEntry, JournaledString, JSTree, JSTNode, DeltaMap,
-insert!, delete_at!, structure_variation!, copy_number_variation!,
-flatten, print_tree, print_sequences, build_sequences, print_deltas,
-add_delta!, remove_delta!, apply_delta, add_node, remove_node!,
-approximate_findall, approximate_search, exact_search,
-get_mutation_history, get_mutation_interval, get_sequences_at_time,
-simulate_mutation!, remove_mutation!, is_equal, print_results, trim_node,
-load_fasta, write_fasta, load_fastq, write_fastq
+    DeltaTypeCNV, JournalEntry, JournaledString, JSTree, JSTNode, DeltaMap,
+    insert!, delete_at!, structure_variation!, copy_number_variation!,
+    flatten, print_tree, print_sequences, build_sequences, print_deltas,
+    add_delta!, remove_delta!, apply_delta, add_node, remove_node!,
+    approximate_findall, approximate_search, exact_search,
+    get_mutation_history, get_mutation_interval, get_sequences_at_time,
+    simulate_mutation!, remove_mutation!, is_equal, print_results, trim_node,
+    load_fasta, write_fasta, load_fastq, write_fastq
 end
+
+### BioJournls.jl ends here
+
+
