fb2d6b581b04957cb773bf67ecea1a18d698772e
Marco Antoniotti <marco.antoniotti@unimib.it>
2025-04-26 21:56:22 +0200

    Minor changes.

--- src/BioJournals.jl
+++ src/BioJournals.jl
@@ -1,3 +1,5 @@
+### -*- Mode: Julia -*-
+
 ### BioJournals.jl
 
 module BioJournals
@@ -13,17 +15,58 @@ include("functions.jl")
 include("algorithms.jl")
 include("io.jl")
 
-export DeltaType, DeltaTypeDel, DeltaTypeIns, DeltaTypeSnp, DeltaTypeSV, 
-    DeltaTypeCNV, JournalEntry, JournaledString, JSTree, JSTNode, DeltaMap,
-    insert!, delete_at!, structure_variation!, copy_number_variation!,
-    flatten, print_tree, print_sequences, build_sequences, print_deltas,
-    add_delta!, remove_delta!, apply_delta, add_node, remove_node!,
-    approximate_findall, approximate_search, exact_search,
-    get_mutation_history, get_mutation_interval, get_sequences_at_time,
-    simulate_mutation!, remove_mutation!, is_equal, print_results, trim_node,
-    load_fasta, write_fasta, load_fastq, write_fastq
+export DeltaType,
+    DeltaTypeDel,
+    DeltaTypeIns,
+    DeltaTypeSnp,
+    DeltaTypeSV, 
+    DeltaTypeCNV,
+
+    JournalEntry,
+    JournaledString,
+    JSTree,
+    JSTNode,
+
+    DeltaMap,
+    
+    insert!,
+    delete_at!,
+    structure_variation!,
+    copy_number_variation!,
+    
+    flatten,
+    print_tree,
+    print_sequences,
+    build_sequences,
+    print_deltas,
+    
+    add_delta!,
+    remove_delta!,
+    apply_delta,
+    add_node,
+    remove_node!,
+    
+    approximate_findall,
+    approximate_search,
+    exact_search,
+    
+    get_mutation_history,
+    get_mutation_interval,
+    get_sequences_at_time,
+    
+    simulate_mutation!,
+    remove_mutation!,
+    is_equal,
+    print_results,
+    trim_node,
+    
+    load_fasta,
+    write_fasta,
+    load_fastq,
+    write_fastq
 end
 
-### BioJournls.jl ends here
+### BioJournls.jl ends here.
+
 
 
